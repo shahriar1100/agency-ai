7f361e8b4b717707a02cbb10d4c081797a55b607
alshahriar-dev <sahariarsikder.aa@gmail.com>
2026-02-19 23:07:06 +0600

    contact up section complete

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import TrustedBy from './components/TrustedBy'
 import Services from './components/Services'
 import OurWork from './components/OurWork'
 import Teams from './components/Teams'
+import ContactUs from './components/ContactUs'
 
 const App = () => {
   const [theme , setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light')
@@ -17,6 +18,7 @@ const App = () => {
       <Services/>
       <OurWork/>
       <Teams/>
+      <ContactUs/>
     </div>
     </>
   )
--- src/components/OurWork.jsx
+++ src/components/OurWork.jsx
@@ -25,7 +25,7 @@ const OurWork = () => {
   return (
     <div
       id="ourWork"
-      className="flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-23 xl:px-40 pt-30 text-gray-700 dark:text-white"
+      className="flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white"
     >
         <Title title='Our Latest Work' desc='From strategy to execution, we craft digital solutions that move your business forward.'/>
         
