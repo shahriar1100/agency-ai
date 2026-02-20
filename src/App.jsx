13b884b20a790c974c486c630d526a70343ca8ca
alshahriar-dev <sahariarsikder.aa@gmail.com>
2026-02-20 11:06:21 +0600

    mouse moving smoth

--- src/App.jsx
+++ src/App.jsx
@@ -61,6 +61,7 @@ const App = () => {
 
         {/* Custom Cursor Ring */}
         <div
+          style={{transition: 'transform 0.1s ease-out'}}
           ref={outlineRef}
           className="fixed top-0 left-0 h-10 w-10 rounded-full border border-primary pointer-events-none z-[9999]"
         ></div>
